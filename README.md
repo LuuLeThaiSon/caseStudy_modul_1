b13d22aeee0b2aafcbacba0b78d1eb760b3ff4dc
LuuLeThaiSon <llts28497@gmail.com>
2022-09-25 22:47:10 +0700

    DONE

---
+++ README.md
@@ -0,0 +1,3 @@
+CaseStudy module 1
+game đoán số
+số lần đoán: 10 lần
---
+++ app.js
@@ -0,0 +1,51 @@
+var randomNumber = parseInt((Math.random()*100)+1);
+// var randomNumber = 55;
+
+var guessed_num = [];
+var count = 10;
+
+var result = document.getElementById("result");
+
+function check() {
+     var number = +document.getElementById("number").value;
+
+     if (number < 1 || number > 100) {
+          result = "Hãy nhập số trong khoảng từ 1 đến 100"
+     } else {
+          guessed_num.push(number)
+          count--;
+          console.log(guessed_num);
+          if (number > randomNumber) {
+               result = "Hãy nhập số bé hơn"
+               document.getElementById("guessed").innerHTML = "Các số đã nhập: " + guessed_num;
+               document.getElementById("remain").innerHTML = "Số lần còn lại " + count;
+          } else if (number < randomNumber) {
+               result = "Hãy nhập số lớn hơn"
+               document.getElementById("guessed").innerHTML = "Các số đã nhập: " + guessed_num;
+               document.getElementById("remain").innerHTML = "Số lần còn lại " + count;
+          } else {
+               result = "You Win!!!!"
+               document.getElementById("guessBtn").disabled = true;
+               document.getElementById("guessed").innerHTML = "Các số đã nhập: " + guessed_num;
+               document.getElementById("remain").innerHTML = "Số lần còn lại " + count;
+          }
+     }
+
+     if (count==0 && number!=randomNumber) {
+          document.getElementById("remain").innerHTML = "Số lần còn lại " + count;
+          result = "You Lose!!!"
+          document.getElementById("guessBtn").disabled = true;
+     }
+
+     document.getElementById("result").innerHTML= result;
+
+}
+
+function restart() {
+     location.reload();
+}
+
+
+
+     
+
